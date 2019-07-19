ef75251750eb543df0c1af6865c957ce233bb6df
Lafe Spietz <lafelabs@gmail.com>
2019-07-19 16:13:18 -0400

    thing replicates to thing

--- mkdir.php
+++ mkdir.php
@@ -5,7 +5,7 @@ $dirname = $_GET["dir"];//get dir
 
 mkdir($dirname);
 
-copy("replicator.php",$dirname."/replicator.php");
+copy("https://raw.githubusercontent.com/LafeLabs/thing/master/php/replicator.txt","symbol/replicator.php");
 
 echo "<a href = \"".$dirname."/replicator.php\">".$dirname."/replicator.php</a>";
 
