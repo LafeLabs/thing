2232cf0eb8f08586a6ed2e2baa52d002bb15c43a
Lafe Spietz <lafelabs@gmail.com>
2019-12-14 13:53:19 -0500

    added a js library file that has the unicodemap in it

---
+++ symbol/data/unicodemap.js
@@ -0,0 +1,18 @@
+unicodemap = [
+    {
+        "ascii":"s",
+        "character":"下"
+    },
+    {
+        "ascii":"a",
+        "character":"上"
+    },
+    {
+        "ascii":"j",
+        "character":"小"
+    },
+    {
+        "ascii":"k",
+        "character":"大"
+    }
+];
