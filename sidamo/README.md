3289cfbfafd6f9043c29319c5b3f00d21c9e4619
Lafe Spietz <lafelabs@gmail.com>
2020-04-05 17:07:44 -0400

    created sidamo/ in thing/

---
+++ sidamo/README.md
@@ -0,0 +1,9 @@
+### [link up to Lafe's Raspberry Pi](../../)
+
+### [link up to fultonmd dot net](../)
+
+# Sidamo
+
+Edit this page by clicking the edit link, then return to scroll by clicking the scroll link.
+
+
---
+++ sidamo/replicator.php
@@ -0,0 +1,13 @@
+<?php
+
+$dnaurl = "https://raw.githubusercontent.com/LafeLabs/thing/master/data/dna.txt";
+
+copy("https://raw.githubusercontent.com/LafeLabs/thing/master/sidamo/README.md","README.md");
+
+?>
+<a href = "index.html">CLICK TO GO TO PAGE</a>
+<style>
+a{
+    font-size:3em;
+}
+</style>
