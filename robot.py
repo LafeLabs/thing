bae0f81d418c127fb89e45db624e6afb44a4b214
Lafe Spietz <lafelabs@gmail.com>
2020-04-25 13:30:56 -0400

    robot.py works, but only when run from command line, not from server

--- robot.py
+++ robot.py
@@ -1,9 +1,162 @@
+#robotExists = True #python has capitalized booleans because it is dumb
+robotExists = False #python has capitalized booleans because it is dumb
+
+from time import sleep # Import the sleep function from the time module
+
+if robotExists:
+    import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+    GPIO.setwarnings(False) # Ignore warning for now
+    GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+
+if not robotExists:
+    print("no robot here")
+
+if robotExists:
+    print("there is a robot i hate python")
+
 import json
+
+dirPin1 = 16
+stepPin1 = 20
+enPin1 = 21
+dirPin2 = 8
+stepPin2 = 7
+enPin2 = 1
+dirPin3 = 14
+stepPin3 = 15
+enPin3 = 18
+stepTime = 0.001
+
+
+if robotExists:
+    GPIO.setup(dirPin1, GPIO.OUT, initial=GPIO.LOW) # 
+    GPIO.setup(stepPin1, GPIO.OUT, initial=GPIO.LOW) # 
+    GPIO.setup(enPin1, GPIO.OUT, initial=GPIO.HIGH) # 
+    GPIO.setup(dirPin2, GPIO.OUT, initial=GPIO.LOW) # 
+    GPIO.setup(stepPin2, GPIO.OUT, initial=GPIO.LOW) # 
+    GPIO.setup(enPin2, GPIO.OUT, initial=GPIO.HIGH) # 
+    GPIO.setup(dirPin3, GPIO.OUT, initial=GPIO.LOW) # 
+    GPIO.setup(stepPin3, GPIO.OUT, initial=GPIO.LOW) # 
+    GPIO.setup(enPin3, GPIO.OUT, initial=GPIO.HIGH) # 
+
+def moveLeft(numSteps):
+    if robotExists:
+        GPIO.output(dirPin1,GPIO.LOW)
+        GPIO.output(dirPin3,GPIO.HIGH) 
+        GPIO.output(enPin1, GPIO.LOW) # enable  
+        GPIO.output(enPin3, GPIO.LOW) # enable  
+        GPIO.output(enPin2, GPIO.HIGH) # deenable  
+        for x in range(abs(numSteps)):
+            GPIO.output(stepPin1, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin1, GPIO.LOW) # Turn off
+            sleep(stepTime) # 
+            GPIO.output(stepPin3, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin3, GPIO.LOW) # Turn off
+            sleep(stepTime) #       
+        GPIO.output(enPin1, GPIO.HIGH) # de-enable  
+        GPIO.output(enPin3, GPIO.HIGH) # de-enable  
+    else:
+        print("move left " + str(numSteps))
+        
+def moveRight(numSteps):
+    if robotExists:
+        GPIO.output(dirPin1,GPIO.HIGH)
+        GPIO.output(dirPin3,GPIO.LOW) 
+        GPIO.output(enPin1, GPIO.LOW) # enable  
+        GPIO.output(enPin3, GPIO.LOW) # enable  
+        GPIO.output(enPin2, GPIO.HIGH) # deenable  
+        for x in range(abs(numSteps)):
+            GPIO.output(stepPin1, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin1, GPIO.LOW) # Turn off
+            sleep(stepTime) # 
+            GPIO.output(stepPin3, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin3, GPIO.LOW) # Turn off
+            sleep(stepTime) #       
+        GPIO.output(enPin1, GPIO.HIGH) # de-enable  
+        GPIO.output(enPin3, GPIO.HIGH) # de-enable  
+    else:
+        print("move right " + str(numSteps))
+
+def moveUp(numSteps):
+    if robotExists:
+        GPIO.output(dirPin1,GPIO.HIGH)
+        GPIO.output(dirPin3,GPIO.HIGH)
+        GPIO.output(enPin2, GPIO.HIGH) # deenable  
+        GPIO.output(enPin1, GPIO.LOW) # enable  
+        GPIO.output(enPin3, GPIO.LOW) # enable  
+        for x in range(abs(numSteps)):
+            GPIO.output(stepPin1, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin1, GPIO.LOW) # Turn off
+            sleep(stepTime) # 
+            GPIO.output(stepPin3, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin3, GPIO.LOW) # Turn off
+            sleep(stepTime) #       
+        GPIO.output(enPin1, GPIO.HIGH) # de-enable  
+        GPIO.output(enPin3, GPIO.HIGH) # de-enable  
+    else:
+        print("move up " + str(numSteps))
+
+def moveDown(numSteps):
+    if robotExists:
+        GPIO.output(dirPin1,GPIO.LOW)
+        GPIO.output(dirPin3,GPIO.LOW)
+        GPIO.output(enPin2, GPIO.HIGH) # deenable  
+        GPIO.output(enPin1, GPIO.LOW) # enable  
+        GPIO.output(enPin3, GPIO.LOW) # enable  
+        for x in range(abs(numSteps)):
+            GPIO.output(stepPin1, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin1, GPIO.LOW) # Turn off
+            sleep(stepTime) # 
+            GPIO.output(stepPin3, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin3, GPIO.LOW) # Turn off
+            sleep(stepTime) #       
+        GPIO.output(enPin1, GPIO.HIGH) # de-enable  
+        GPIO.output(enPin3, GPIO.HIGH) # de-enable  
+    else:
+        print("move down " + str(numSteps))
+
+def moveIn(numSteps):
+    if robotExists:
+        GPIO.output(dirPin2,GPIO.LOW)
+        GPIO.output(enPin2, GPIO.LOW) # enable
+        GPIO.output(enPin3, GPIO.HIGH) # deenable    
+        GPIO.output(enPin1, GPIO.HIGH) # deenable  
+        for x in range(abs(numSteps)):
+            GPIO.output(stepPin2, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin2, GPIO.LOW) # Turn off
+            sleep(stepTime) # 
+        GPIO.output(enPin2, GPIO.HIGH) # de-enable  
+    else:
+        print("move in " + str(numSteps))
+
+def moveOut(numSteps):
+    if robotExists:
+        
+        GPIO.output(dirPin2,GPIO.HIGH) # direction
+        GPIO.output(enPin2, GPIO.LOW) # enable
+        GPIO.output(enPin3, GPIO.HIGH) # deenable    
+        GPIO.output(enPin1, GPIO.HIGH) # deenable  
+        for x in range(abs(numSteps)):
+            GPIO.output(stepPin2, GPIO.HIGH) # Turn on
+            sleep(stepTime) #  
+            GPIO.output(stepPin2, GPIO.LOW) # Turn off
+            sleep(stepTime) # 
+        GPIO.output(enPin2, GPIO.HIGH) # de-enable  
+    else:
+        print("move out " + str(numSteps))
 
 robotfile = open("data/robot.txt", "r")
 robotjson = json.loads(robotfile.read())
 robotfile.close()
-
 
 hypercubefile = open("jscode/hypercube.js", "r")
 hypercubestring = hypercubefile.read();
@@ -26,21 +179,6 @@ for row in bytecode:
     if len(splitrow) > 0:
         address = int(splitrow[0],8)
         hypercube[address] = splitrow[1]
-
-def moveLeft(x):
-    print("move left " + str(x))
-    #do the thing on the gpio here
-def moveRight(x):
-    print("move right " + str(x))
-def moveIn(x):
-    print("move in " + str(x))
-def moveOut(x):
-    print("move out " + str(x))
-def moveUp(x):
-    print("move up " + str(x))
-def moveDown(x):
-    print("move down " + str(x))
-
 
 def action(address):
     global numSteps
@@ -74,6 +212,15 @@ def sequence(glyph):
               action(address)
             if address >= 0o500 and address < 0o600:
               sequence(hypercube[address])
+#moveDown(20)
+#moveIn(100)
+#moveLeft(100)
+#moveOut(100)
+#moveRight(100)
+#moveUp(20)
 
 sequence(mainglyph)
-#sequence("0500,0407,0407,0500,0407,0407,0500,")
+
+#sequence("0500,")
+
+
