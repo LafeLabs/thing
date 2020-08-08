c9442c8a48fc330c3e1f56a1a9ccd3f9164859c2
Lafe Spietz <lafelabs@gmail.com>
2020-08-07 23:09:41 -0400

    tiny tweaks to map editor for canvas functionality

--- jscode/mapfactory.js
+++ jscode/mapfactory.js
@@ -67,6 +67,7 @@ function Map(w,h,div) {
                 newcan.style.position = "absolute";
                 newcan.style.left = "0px";
                 newcan.style.top = "0px";
+                newcan.class = "geometroncanvas";
                 //GVM(x0,y0,unit,theta0,canvas2d,width,height,bytecode)
                 var newg = new GVM(this.array[index].geometron.x0rel*this.array[index].w*this.w,this.array[index].geometron.y0rel*this.array[index].w*this.w,this.array[index].geometron.unitrel*this.array[index].w*this.w,this.array[index].geometron.theta0,newcan,this.array[index].w*this.w,this.array[index].w*this.array[index].geometron.height*this.w/this.array[index].geometron.width,hypercube);
                 newg.importbytecode(this.array[index].geometron.shapes);
